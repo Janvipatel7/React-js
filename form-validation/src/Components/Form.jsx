7e0901847c1998edaa60a7a4482178bd1b7277e0
janvipatel7 <janvi72004@gmail.com>
2025-05-31 12:42:30 +0530

    half validation done

--- form-validation/src/Components/Form.jsx
+++ form-validation/src/Components/Form.jsx
@@ -2,42 +2,83 @@ import React, { useState } from 'react'
 
 const Form = () => {
 
-    const [input , setInput] = useState({})
+    const [input , setInput] = useState({
+        fname:'',lname:'', email:'',phone:'',password:'',confirmPassword:''
+    })
+
+    const [errors , setErrors] = useState({})
 
     const handleChange = (e) =>{
         console.log(e.target.value)
         setInput({...input , [e.target.id] : e.target.value})
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        let validationError = {}
+
+        if(fname.value.trim() == ""){
+            validationError.fname = "Please Enter valid First Name....";
+        }
+
+        if(lname.value.trim() == ""){
+            validationError.lname = "Please Enter valid Last Name...."
+        }
+
+        if(password.value.trim() == "" || password.value <= 8){
+            validationError.password = "Please Enter Strong Password...."
+        }
+
+        if(confirmPassword.value.trim() == "" || password.value == confirmPassword.value){
+            validationError.confirmPassword = "Match Password...."
+        }
+
+        setErrors(validationError)
+         
     }
     console.log(input)
     return (
         <>
             <div className='container mx-auto my-5 md:px-3 p-10'>
                 <h1 className='text-4xl font-semibold text-center my-10'>Form Validation</h1>
-                <form>
+                <form onClick={handleSubmit}>
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
-                            <label htmlFor="fname" className="block mb-2 text-sm font-medium text-gray-900 ">First name</label>
-                            <input onChange={handleChange} type="text" id="fname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required />
+                            <label htmlFor="fname" className="block mb-2 text-sm font-medium text-gray-900">First name</label>
+                            <input onChange={handleChange} value={input.fname}  type="text" id="fname" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 ${errors.fname ? "bg-red-50 border border-red-500" : ""}` } placeholder="John" required />
+                            {
+                                errors.fname && <p class="mt-2 text-sm text-red-600 dark:text-red-500 font-medium">{errors.fname}</p>
+                            }
                         </div>
                         <div>
-                            <label htmlFor="lname" className="block mb-2 text-sm font-medium text-gray-900 ">Last name</label>
-                            <input onChange={handleChange} type="text" id="lname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Doe" required />
+                            <label htmlFor="lname" className="block mb-2 text-sm font-medium text-gray-900">Last name</label>
+                            <input onChange={handleChange} value={input.lname} type="text" id="lname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5" placeholder="Doe" required />
+                            {
+                                errors.lname && <p class="mt-2 text-sm text-red-600 dark:text-red-500 font-medium">{errors.lname}</p>
+                            }
                         </div>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email address</label>
-                            <input onChange={handleChange} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required />
+                            <input onChange={handleChange} value={input.email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5" placeholder="john.doe@company.com" required />
                         </div>
                         <div>
                             <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 ">Phone number</label>
-                            <input onChange={handleChange} type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                            <input onChange={handleChange} value={input.phone} type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                            <input onChange={handleChange} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required />
+                            <input onChange={handleChange} value={input.password} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5" placeholder="•••••••••" required />
+                            {
+                                errors.password && <p class="mt-2 text-sm text-red-600 dark:text-red-500 font-medium">{errors.password}</p>
+                            }
                         </div>
                         <div>
                             <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
-                            <input onChange={handleChange} type="password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required />
+                            <input onChange={handleChange} value={input.confirmPassword} type="password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5" placeholder="•••••••••" required />
+                            {
+                                errors.confirmPassword && <p class="mt-2 text-sm text-red-600 dark:text-red-500 font-medium">{errors.confirmPassword}</p>
+                            }
                         </div>
 
                     </div>
