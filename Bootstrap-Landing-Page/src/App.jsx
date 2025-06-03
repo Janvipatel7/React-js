28db7c69879d40ee16aea1e001a3f5b00df283ed
janvipatel7 <janvi72004@gmail.com>
2025-06-03 12:09:10 +0530

    add new project

---
+++ Bootstrap-Landing-Page/src/App.jsx
@@ -0,0 +1,11 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => {
+  return (
+    <div>App
+      <button className='btn btn-danger'>submit</button>
+    </div>
+  )
+}
+
+export default App
