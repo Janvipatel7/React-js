a74302c160b9775916a70b9f843fdbdf03873971
janvipatel7 <janvi72004@gmail.com>
2025-06-06 11:50:34 +0530

    review card add

--- Review-Box/src/App.jsx
+++ Review-Box/src/App.jsx
@@ -1,12 +1,11 @@
 
-import Card from './assets/Components/Card'
+
 import Review from './assets/Components/Review'
 
 const App = () => {
   return (
     <div>
         <Review />
-        <Card />
     </div>
   )
 }
--- Review-Box/src/assets/Components/Review.jsx
+++ Review-Box/src/assets/Components/Review.jsx
@@ -6,12 +6,8 @@ const Review = () => {
     });
 
     const [errors, setErrors] = useState({});
-
     const formRef = useRef(null);
-
-    const[card , setCard] = useState(false);
-
-    const[allData , setAllData] = useState([]);
+    const [allData, setAllData] = useState([]);
 
     const handleChange = (e) => {
         setInput({ ...input, [e.target.id]: e.target.value });
@@ -37,38 +33,67 @@ const Review = () => {
         }
 
         setErrors(reviewError);
-        if(Object.keys(reviewError).length === 0){
-            setCard(true);
-            setAllData([...allData , input]);
+
+        if (Object.keys(reviewError).length === 0) {
+            setAllData([...allData, input]);
+            setInput({
+                name: "", message: "", date: "", rate: "",
+            });
         }
-       
+
+        console.log(input);
     };
 
     return (
-        <div className='container mx-auto my-5 px-4'>
+        <div className={`container mx-auto my-5 px-4 ${allData.length === 0 ? "justify-center items-center" : ""}`}>
             <form ref={formRef} onSubmit={handleSubmit} className="mx-auto bg-white border p-8 rounded-xl shadow-2xl max-w-lg w-full">
                 <div className="mb-5">
                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
-                    <input type="text" value={input.name} onChange={handleChange} id="name" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter Your Name" />
+                    <input
+                        type="text"
+                        value={input.name}
+                        onChange={handleChange}
+                        id="name"
+                        className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                        placeholder="Enter Your Name"
+                    />
                     {errors.name && <p className="mt-2 text-sm text-red-600">{errors.name}</p>}
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
-                    <textarea id="message" value={input.message} onChange={handleChange} rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Leave a comment..." />
+                    <textarea
+                        id="message"
+                        value={input.message}
+                        onChange={handleChange}
+                        rows={4}
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                        placeholder="Leave a comment..."
+                    />
                     {errors.message && <p className="mt-2 text-sm text-red-600">{errors.message}</p>}
                 </div>
 
                 <div className='grid gap-6 mb-6 md:grid-cols-2'>
                     <div>
                         <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900">Date</label>
-                        <input type="date" id="date" value={input.date} onChange={handleChange} className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
+                        <input
+                            type="date"
+                            id="date"
+                            value={input.date}
+                            onChange={handleChange}
+                            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                        />
                         {errors.date && <p className="mt-2 text-sm text-red-600">{errors.date}</p>}
                     </div>
 
                     <div>
                         <label htmlFor="rate" className="block mb-2 text-sm font-medium text-gray-900">Rate</label>
-                        <select id="rate" value={input.rate} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+                        <select
+                            id="rate"
+                            value={input.rate}
+                            onChange={handleChange}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                        >
                             <option value="">----Select Rate----</option>
                             <option value={1}>1</option>
                             <option value={2}>2</option>
@@ -86,12 +111,23 @@ const Review = () => {
                 </div>
             </form>
 
-            <div>
-                <div className='flex flex-wrap gap-5 justify-center'>
-                    {allData.map((review , index) =>{
-                        
-                    })}
-                </div>
+            <div className='flex flex-wrap gap-5 justify-center mt-10'>
+                {allData.map((review, index) => (
+                    <div key={index} className="bg-gradient-to-br from-[#b4857d] to-[#b4857d] text-white p-4 rounded-xl shadow-md ">
+                        <div className=" font-semibold text-2xl">{review.name}</div>
+                        <div className="opacity-90 mt-1 text-1xl">{review.date}</div>
+                        <p className="italic text-sm leading-relaxed mb-4">
+                            "{review.message}"
+                        </p>
+                        <div className="flex gap-1 mb-3">
+                            {"⭐".repeat(Number(review.rate))}
+                        </div>
+                        <div className='flex gap-6'>
+                            <button className="text-[#b4857d] bg-white hover:bg-[#b4857d] hover:text-white transition-all duration-300 ease-in-out border border-[#b4857d] font-medium rounded-lg text-sm px-5 py-2.5 text-center">Edit</button>
+                            <button className="text-[#b4857d] bg-white hover:bg-[#b4857d] hover:text-white transition-all duration-300 ease-in-out border border-[#b4857d] font-medium rounded-lg text-sm px-5 py-2.5 text-center">Delete</button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
