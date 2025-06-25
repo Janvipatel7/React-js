f2451a4fc0f98ce42032a98649549014bae1e182
Janvipatel7 <janvi72004@gmail.com>
2025-06-25 16:35:38 +0530

    some changes

--- Bootstrap-Landing-Page/src/Components/SpecialFeatures.jsx
+++ Bootstrap-Landing-Page/src/Components/SpecialFeatures.jsx
@@ -1,21 +1,34 @@
 import ImageContent from "./ImageContent"
 
 const SpecialFeatures = () => {
-    let heading = `Sandy's special features.`
-    let content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's typesetting industry standard dummy text ever since the typesetting."
-    let btn1 = "Learn more"
-    let btn2 = "Download now"
+    
+    
+
 
     return (
         <>
             <section className="relative special-feature-image pt-100 pb-100">
-                <div className="color-overlay "></div> 
+                <div className="color-overlay "></div>
                 <div className="container position-relative">
                     <div className="row justify-content-center">
                         <div className="col-md-8">
-                           <div className="feature">
-                                 <ImageContent heading={heading} content={content} btn1={btn1} btn2={btn2} />
-                           </div>
+                            <div className="feature">
+                                <div className="hero-content">
+                                    <div className="mb-4">
+                                        <h1 className="fs-50 text-capitalize text-center text-white">Sandy's special features.</h1>
+                                    </div>
+                                    <div className="mb-5">
+                                        <p className="text-center text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's typesetting industry standard dummy text ever since the typesetting.</p>
+                                    </div>
+                                    <div className="d-flex justify-content-center gap-5">
+                                        <button className="primary-btn">Learn more</button>
+                                        <button className="black-btn">Download now</button>
+                                    </div>
+                                    <div className="">
+                                        {/* <img src="/public/Image/bg-pattern-light.png" alt="" /> */}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
