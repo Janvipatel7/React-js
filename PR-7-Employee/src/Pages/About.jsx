f979bab70074e65ed30ccfb6cf2f1a5431ca0a84
Janvipatel7 <janvi72004@gmail.com>
2025-07-15 16:05:08 +0530

    useReducer

--- PR-7-Employee/src/Pages/About.jsx
+++ PR-7-Employee/src/Pages/About.jsx
@@ -6,7 +6,6 @@ const About = () => {
       <section>
           <div className="container mx-auto py-4">
           <h1 className="text-teal-600 font-semibold text-2xl"></h1>
-          
       </div>
       </section>
     </>
---
+++ UseReducer/src/App.jsx
@@ -0,0 +1,49 @@
+import { useReducer } from "react"
+
+const initialState = {
+    users :[
+        { id : 1 , name : "shreya"},
+        { id : 2 , name : "anjan"},
+        { id : 3 , name : "aryan"}
+    ]
+}
+
+const reducer = (state , action) => {
+  switch (action.type) {
+    case  "CLEAR_ALL" :
+        return {
+          users : []
+        }
+  }
+}
+
+const App = () => {
+
+  const [state , dispatch] = useReducer(reducer , initialState);
+
+  const handleClear = () => {
+      dispatch({ type : "CLEAR_ALL"})
+  }
+  
+  const handleDelete = () => {
+
+  }
+  
+  return (
+    <div>
+        {
+          state.users.map((user) => {
+             return <div key={user.id}>
+                <h1>{user.name}</h1>
+                <button onClick={handleDelete}>x</button>
+              </div>
+          })
+        }
+
+        <br></br>
+        <button onClick={handleClear} >clear</button>
+    </div>
+  )
+}
+
+export default App
